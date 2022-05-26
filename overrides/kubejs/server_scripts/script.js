4bd96ab945f5929cfed0572cece92fcadc53c55e
Call me Kall <76616758+Kasualix@users.noreply.github.com>
2022-05-26 12:46:13 +0800

    Delete overrides directory

--- overrides/kubejs/server_scripts/script.js
+++
@@ -1,8 +0,0 @@
-onEvent('block.right_click', event => {
-	if (event.block == 'minecraft:enchanting_table') {
-		if (!event.player.stages.has('three')) {
-			event.server.runCommand(`say 你需要击败法老并将其掉落的神器放入背包才能使用附魔台`)
-			event.server.runCommandSilent(`fill ${event.block.x} ${event.block.y} ${event.block.z} ${event.block.x} ${event.block.y} ${event.block.z} air`)
-		}
-	}
-})
