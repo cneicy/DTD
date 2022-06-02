a18eb55b0eb304f80976b504a545c9cdfe39da00
Eicy <32791896+cneicy@users.noreply.github.com>
2022-06-02 16:36:22 +0800

    1.6.4

--- overrides/kubejs/server_scripts/general.js
+++ overrides/kubejs/server_scripts/general.js
@@ -1,28 +1,3 @@
-const minX = 1000;
-const minZ = 1000;
-const maxX = 10000;
-const maxZ = 10000;
-const minY = -200;
-const maxY = 200;
-const reverseNumber = true;
-function getNumber(min,max){
-    let randNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
-    if(Math.random() > 0.5 && reverseNumber){
-        randNumber = -randNumber;
-    }
-    return randNumber;
-}
-onEvent('player.tick',function (event){
-    if(event.player.stages.has('cheat')){
-        event.player.setPosition(getNumber(minX,maxX), Math.abs(getNumber(minY,maxY)), getNumber(minZ,maxZ));
-        event.player.potionEffects.add('minecraft:regeneration', 114514, 255, false, false);
-        event.player.potionEffects.add('minecraft:slowness', 114514, 255, false, false);
-        event.player.potionEffects.add('minecraft:nausea', 114514, 255, false, false);
-        event.player.potionEffects.add('minecraft:blindness', 114514, 255, false, false);
-        event.server.runCommandSilent(`clear @a`);
-        event.server.runCommandSilent(`gamemode survival @a`);
-    }
-})
 
 onEvent('block.right_click', event => {
 	if (event.block == 'minecraft:enchanting_table') {
